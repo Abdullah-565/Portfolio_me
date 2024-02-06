5e80fbbcbc0edcba9796319484b1b2832db3c6cd
Abdullah-565 <rajaabdullah2112154@gmail.com>
2024-02-06 17:11:35 +0500

    changes in project.tsx

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -93,7 +93,7 @@ const Project = () => {
           <Image
             alt="gallery"
             className="absolute inset-0 w-full h-full object-cover object-center"
-            src={require("../../../public/assets/Clock.PNG")}
+            src={require("../../../public/assets/Clock.png")}
           />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-[#000021] opacity-0 hover:opacity-100">
             <h2 className="tracking-widest text-sm title-font font-medium text-red-500 mb-1">
